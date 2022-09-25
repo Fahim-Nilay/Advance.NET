103bd41d6b53745e219f90dee3f9c5d72528e53b
Fahim Hasan Nilay <fahimnilay@gmail.com>
2022-09-25 21:20:11 +0600

    v1.0

---
+++ First Project/FirstProject/FirstProject/Controllers/CvHomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace FirstProject.Controllers
+{
+    public class CvHomeController : Controller
+    {
+        // GET: CvHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Intro()
+        {
+            return View();
+        }
+        public ActionResult PersonalInfo() 
+        {
+            return View();
+        }
+        public ActionResult EduBackground ()
+        {
+            return View();
+        }
+        
+        public ActionResult Skills()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
